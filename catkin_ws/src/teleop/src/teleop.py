3c3dbf2626f698312d6c9eac8a7323b4c9bb4b2c
mael.lefloch10@gmail.com <mael.lefloch10@gmail.com>
2025-02-03 16:26:36 +0000

    second commit

--- catkin_ws/src/teleop/src/teleop.py
+++ catkin_ws/src/teleop/src/teleop.py
@@ -8,36 +8,38 @@ class VelocityControl:
 
     def __init__(self):
         rospy.init_node('velocity_control')
-        self.pepper_publisher = rospy.Publisher('cmd_vel', Twist, queue_size=1000)
-        self.joy_subscriber = rospy.Subscriber('/joy', Joy, self.joy_callback, queue_size=1000)
+        self.pepper_publisher = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+        self.joy_subscriber = rospy.Subscriber('/joy', Joy, self.joy_callback, queue_size=10)
         self.linear_scale = 1.0
         self.angular_scale = 1.0
         self.reel_coef = 0.2
-
+        self.wait_for_joy = 0
+        self.last_twist = Twist()
+        
+        # Timer pour publier à une fréquence fixe
+        self.publish_rate = rospy.Rate(10)  # 10 Hz
+        self.timer = rospy.Timer(rospy.Duration(0.1), self.publish_velocity)
+    
     def joy_callback(self, joy):
-        # Adjust axes values based on deadzone (0.15 threshold)
+        # Ajuster les valeurs des axes en fonction d'une zone morte (seuil 0.15)
+        self.wait_for_joy = 1
         axes = list(joy.axes)
         for i in range(len(axes)):
             if abs(axes[i]) < 0.15:
                 axes[i] = 0.0
 
-        # Calculate linear and angular speeds
-        linear_speed_x = axes[1] * self.linear_scale  # Left joystick for linear velocity
-        linear_speed_y = axes[0] * self.linear_scale  # Right joystick for lateral velocity
-        angular_speed_z = axes[3] * self.angular_scale  # Right joystick for angular velocity
+        # Calculer les vitesses linéaires et angulaires
+        linear_speed_x = axes[1] * self.linear_scale  # Joystick gauche pour la vitesse linéaire
+        angular_speed_z = axes[3] * self.angular_scale  # Joystick droit pour la vitesse angulaire
 
-        # Create Twist message with linear and angular velocities
-        twist_cmd = Twist()
-        twist_cmd.linear.x = linear_speed_x * self.reel_coef
-        twist_cmd.linear.y = 0.0
-        twist_cmd.linear.z = 0.0  # Not used in this example
-        twist_cmd.angular.x = 0.0  # Not used in this example
-        twist_cmd.angular.y = 0.0  # Not used in this example
-        twist_cmd.angular.z = angular_speed_z * self.reel_coef
+        # Mettre à jour la dernière commande de vitesse
+        self.last_twist.linear.x = linear_speed_x * self.reel_coef
+        self.last_twist.angular.z = angular_speed_z * self.reel_coef
 
-        # Publish Twist message
-        self.pepper_publisher.publish(twist_cmd)
-
+    def publish_velocity(self, event):
+        # Publier la dernière commande de vitesse à une fréquence fixe
+        if self.wait_for_joy :
+            self.pepper_publisher.publish(self.last_twist)
 
 def main():
     velocity_control = VelocityControl()
